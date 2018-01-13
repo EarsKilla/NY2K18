30721e05764d84c44b39bebb0e9a9fed98a5de44
Andrey <EarsKilla@users.noreply.github.com>
2018-01-13 17:56:14 +0300

    Update tmonkey_do_fun.js

--- tmonkey_do_fun.js
+++ tmonkey_do_fun.js
@@ -7,21 +7,7 @@
 // @match        *://vk.com/*
 // @updateURL https://earskilla.github.io/NY2K18/tmonkey_do_fun.js
 // ==/UserScript==
-
-/* jshint ignore:start */
-if(typeof console == "object") console.log("domain",location.href);
 var script = document.createElement("script");
 script.setAttribute("type", "text/javascript");
 script.src = "//earskilla.github.io/NY2K18/2k18.js";
 document.head.appendChild(script);
-
-script.onload = function(){
-    setInterval(function(){
-        var x = document.getElementsByClassName("ny2017_link");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].setAttribute("onclick", "Ny2k18.startFlapper(); return false;");
-        }
-    }, 1000);
-};
-/* jshint ignore:end */
