14932d1fbb25ed089f92276abdc79434cd1c8574
Andrey <EarsKilla@users.noreply.github.com>
2018-01-13 18:37:20 +0300

    Update tmonkey_do_fun.js

--- tmonkey_do_fun.js
+++ tmonkey_do_fun.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         2k18
 // @version      1.2
-// @description  try to take pisos in the mouth!
+// @description  try to take bennys in the mouth!
 // @author       EarsKilla#0697
 // @match        *://*.vk.com/*
 // @match        *://vk.com/*
@@ -11,3 +11,9 @@ var script = document.createElement("script");
 script.setAttribute("type", "text/javascript");
 script.src = "//earskilla.github.io/NY2K18/2k18.js";
 document.head.appendChild(script);
+
+window.ny2018ReplaceText = function(e) {
+  return e.replace(/(с новым годом|с наступающим|очько|бутылка|залупа не мытая)([\!]+)?(?=(\s|\,|\<br\>|$))?/i, function(e) {
+    return '<span class="ny2017_link" onClick="Ny2k18.startFlapper(); return false;">' + e + "</span>"
+  })
+};
